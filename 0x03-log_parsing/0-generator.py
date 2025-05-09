3dff7542cf8a46aeedcc2da03d5373a561892606
ahmedosamax <140077048+ahmedosamax@users.noreply.github.com>
2025-05-09 05:38:26 +0300

    fix taskaa

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -2,14 +2,16 @@
 import random
 import sys
 from time import sleep
-import datetime
+from datetime import datetime
 
-for i in range(1000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
+status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+
+for _ in range(10000):
+    ip = ".".join(str(random.randint(0, 255)) for _ in range(4))
+    date = datetime.now().strftime("%d/%b/%Y:%H:%M:%S %z")
+    code = random.choice(status_codes)
+    size = random.randint(1, 1024)
+    log_line = '{} - [{}] "GET /projects/260 HTTP/1.1" {} {}\n'.format(ip, date, code, size)
+    sys.stdout.write(log_line)
     sys.stdout.flush()
+    sleep(random.uniform(0, 0.1))
